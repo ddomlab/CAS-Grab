d905c9fa0951df7d37fc6ac4cda2aac027cfb729
12buntu <kylemhollars@gmail.com>
2024-09-05 23:19:35 -0400

    added function to grab chemical data from pubchem

---
+++ main.py
@@ -0,0 +1,12 @@
+import pubchempy as pcp
+
+
+# a somewhat unnecessary wrapper for pubchem.py
+# CAS numbers fall in the 'name' category on pubchem, so they are searched as names
+# you could also search by the common name or any other synonym, however CAS numbers
+# should return more consistent results,
+def get_compound(CAS):
+    compound_list = pcp.get_compounds(CAS, "name")
+    # TODO throw error if list size is greater than 1
+    compound = compound_list[0]
+    return compound
