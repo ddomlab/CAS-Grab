af0047a7f59b9c33ad9d59d14be375dcd124602a
12buntu <kylemhollars@gmail.com>
2024-09-09 10:31:16 -0400

    removed config.yml and restructured config.py a little bit

--- config.py
+++ config.py
@@ -1,29 +1,33 @@
 import elabapi_python
 import urllib3
-import yaml
-# TODO consider getting rid of config.yml
 
-urllib3.disable_warnings()  # allows the connection
-# configuration file with api key band url
-with open("config.yml") as config_file:
-    config = yaml.safe_load(config_file)
+## CONFIGURATION VARIABLES ##
+# it is best practice to keep your api keys secret. there is a file, 'api_key',
+# in the .gitignore where you should paste your api key as plain text.
+# it will not (and should not) be added to any public git repository, and will remain unique for each machine
+# new api keys can be generated at https://eln.ddomlab.org/ucp.php?tab=4
+API_KEY_PATH = "api_key"
+URL = "https://eln.ddomlab.org/api/v2"
+##################################################
 
-# Configure the api client TODO: move this to a new file
+# allows the connection
+urllib3.disable_warnings()
+
+# takes API key from secret file, loads it to elabapi
+with open(API_KEY_PATH) as keyfile:
+    key = keyfile.read()
 configuration = elabapi_python.Configuration()
-with open(config["api_key_path"]) as keyfile:
-    configuration.api_key["api_key"] = keyfile.read()
+configuration.api_key["api_key"] = key
 configuration.api_key_prefix["api_key"] = "Authorization"
-configuration.host = config["url"]
+configuration.host = URL
 configuration.debug = False
 configuration.verify_ssl = False
 
 # create an instance of the API class
 api_client = elabapi_python.ApiClient(configuration)
+
 # fix issue with Authorization header not being properly set by the generated lib
-with open(config["api_key_path"]) as keyfile:
-    api_client.set_default_header(
-        header_name="Authorization", header_value=keyfile.read()
-    )
+api_client.set_default_header(header_name="Authorization", header_value=key)
 
 
 def load_items_api():
