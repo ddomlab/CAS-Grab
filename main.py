8418d3934dc28c9f0eb8e73960d741782bb1f064
12buntu <kylemhollars@gmail.com>
2024-09-23 15:47:09 -0400

    switched to data matrix

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import frontend.input_process
 p = frontend.input_process.Processor()
 
 while True:
-    p.from_human(input())
+    text = input()
+    if text[0] == "{":
+        p.from_data(text)
+    else:
+        p.from_human(text)
     print(p.registry.id_registry)
