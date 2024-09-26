ac04303ff03b570e65fdcca0273429573c446a50
12buntu <kylemhollars@gmail.com>
2024-09-26 18:04:50 -0400

    very basic ui written

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import frontend.input_process
+import frontend.gui
 
-p = frontend.input_process.Processor()
-
-while True:
-    text = input()
-    if text[0] == "{":
-        p.from_data(text)
-    else:
-        p.from_human(text)
-    print(p.registry.id_registry)
+frontend.gui.main()
