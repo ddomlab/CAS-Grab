1c494b3362b352ac54cd01e85e1de39bcc9ba10a
12buntu <kylemhollars@gmail.com>
2024-10-08 14:25:54 -0400

    minor changes

--- cas_grab/image_generator.py
+++ cas_grab/image_generator.py
@@ -2,7 +2,7 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 
 
-def generate_image(smiles) -> str:
+def generate_image(smiles: str) -> str:
     mol = Chem.MolFromSmiles(smiles)
     filename = "tmp/RDKitImage.png"
     Draw.MolToFile(mol, filename)
